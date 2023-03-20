16b4a9d8fc339d1e67744f9cb966fd25dd48a114
riskalaluna <riskalaluna@gmail.com>
2023-03-20 15:50:14 +0700

    Add project files.

---
+++ latihan4_176/latihan4_176.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+float LuasPersegi(float p, float l) {
+	return p * l;
+}
+float LuasLingkaran(float r) {
+	return 3.14 * r * r;
+}
+float LuasSegitiga(float a, float t) {
+	return 0.5 * a * t;
+}
+
+int main() 
+{
+	float panjang, lebar, jejari, alas, tinggi;
+	int pilihan;
+
+	do {
+		cout << "\n\n====================" << endl;
+		cout << "========MENU=======" << endl;
+		cout << "===================" << endl;
+		cout << "1. Luas Persegi " << endl;
+		cout << "2. Luas Lingkaran " << endl;
+		cout << "3. Luas Segitiga" << endl;
+		cout << "4. Exit" << endl;
+		cout << "Pilihan(1/2/3/4): ";
+		cin >> pilihan;
+
+		switch (pilihan)
+		{
+		case 1:
+			cout << "Masukan Panjang = ";
+			cin >> panjang;
+			cout << "Masukan Lebar = ";
+			cin >> lebar;
+			cout << "Luas Persegi Panjang = " << LuasPersegi(panjang, lebar);
+			break;
+		case 2:
+			cout << "Masukan jari - jari = ";
+			cin >> jejari;
+			cout << "Luas Lingkaran = " << LuasLingkaran(jejari);
+			break;
+		case 3:
+			cout << "Masukan Alas = ";
+			cin >> alas;
+			cout << "Masukan Tinggi = ";
+			cin >> tinggi;
+			cout << "Luas Segitiga = " << LuasSegitiga(alas, tinggi);
+			break;
+		case 4:
+			break;
+		default:
+			cout << "PILIHAN SALAH" << endl;
+			break;
+
+		}
+	} while (pilihan != 4);
+
+
+
+
+
